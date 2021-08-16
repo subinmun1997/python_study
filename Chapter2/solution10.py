9104265ae56483f1f61f729f02bba1fb7e8378a5
subinmun1997 <qzxy812@gmail.com>
2021-08-16 14:16:42 +0900

    문자열 포매팅

---
+++ Chapter2/solution10.py
@@ -0,0 +1,9 @@
+print("I eat %d apples."%3)
+print("I eat %s apples."%"five")
+
+number = 3
+print("I eat %d apples."%number)
+
+number = 10
+day = "three"
+print("I ate %d apples. so I was sick for %s days."%(number, day))
